74330265c4c6e26793a2868270f0ebb9b1dc96f6
Andrei Kondratov <a.kondratov@slurm.io>
2025-02-23 19:53:53 +0500

    init

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("config.lazy")
+require("nvim-tree").setup()
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.syntax = 'on'
+vim.opt.smartindent = true
+vim.opt.autoindent = true
