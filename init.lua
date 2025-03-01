97f41a5f8505d1d2e1a7f7e35426f39df007caac
Andrei Kondratov <a.kondratov@slurm.io>
2025-03-01 17:22:29 +0500

    add gruvbox color theme

--- init.lua
+++ init.lua
@@ -10,3 +10,7 @@ vim.opt.autoindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.scrolloff = 5
+vim.opt.showmode = false
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
