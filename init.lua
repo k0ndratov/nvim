20766cf79c8d5f189f90d3d3522c2530ad22739d
Andrei Kondratov <a.kondratov@slurm.io>
2025-03-01 16:09:06 +0500

    add and configure neotree

--- init.lua
+++ init.lua
@@ -1,8 +1,12 @@
+require("mappings.general")
+require("mappings.neotree")
 require("config.lazy")
-require("nvim-tree").setup()
 
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.syntax = 'on'
 vim.opt.smartindent = true
 vim.opt.autoindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.scrolloff = 5
