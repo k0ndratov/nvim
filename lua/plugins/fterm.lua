5d2a7eb292f343654ab8a503b5dcebbbb8f1dc32
Andrei Kondratov <a.kondratov@slurm.io>
2025-04-29 10:17:23 +0500

    start

--- lua/plugins/fterm.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"numToStr/FTerm.nvim",
-	config = function()
-		vim.keymap.set('n', '<leader>i', '<CMD>lua require("FTerm").toggle()<CR>')
-		vim.keymap.set('t', '<leader>i', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-	end
-}
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,18 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-  	config = function()
-    	require("lualine").setup()
-    end
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require("lualine").setup {
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {}, -- убрали 'branch'
+        lualine_c = {
+          'filename',
+        },
+        lualine_x = {}, -- убрали 'encoding' и 'fileformat' (пингвин)
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+      },
+    }
+  end
 }
